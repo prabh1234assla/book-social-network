2a636ed824e4b3cdebf1035ee6786bb5aae41ac8
prabh1234assla <pdsinghassla@gmail.com>
2024-06-21 23:55:17 +0530

    SecurityFilter added and CORS also added

--- backend/src/main/java/webly/bookstore/backend/BookStoreBackendApplication.java
+++ backend/src/main/java/webly/bookstore/backend/BookStoreBackendApplication.java
@@ -1,7 +1,12 @@
 package webly.bookstore.backend;
 
+import java.util.List;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class BookStoreBackendApplication {
@@ -10,4 +15,17 @@ public class BookStoreBackendApplication {
 		SpringApplication.run(BookStoreBackendApplication.class, args);
 	}
 
+	@Bean
+	public WebMvcConfigurer corsConfigurer(){
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry){
+				registry.
+						addMapping("/**").
+						allowedHeaders("Authorization", "Content-Type").
+						allowedMethods("POST", "GET").
+						allowedOrigins("http://localhost:8000");
+			}
+		};
+	}
 }
